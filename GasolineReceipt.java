3a7d6546be2280e953faa773378a61cea549b274
Ryan Grillo <ryangrillo@gmail.com>
2017-03-25 16:57:12 -0700

    Added some more exercises

---
+++ GasolineReceipt.java
@@ -0,0 +1,20 @@
+package javahard;
+
+public class GasolineReceipt {
+	public static void main(String[] args) {
+			System.out.println( "+--------------------------+" );
+			System.out.println( "|          Las Vegas       |" );
+			System.out.println( "|	 Corner Store      |" );
+			System.out.println( "|                          |" );
+			System.out.println( "|    2017-03-24    4:38pm  |" );
+			System.out.println( "|                          |" );
+            System.out.println( "| Gallons:       10.870    |" );
+            System.out.println( "| Price/gallon: $ 2.589    |" );
+            System.out.println( "|                          |" );
+            System.out.println( "| Fuel total:  $ 22.71     |" );
+            System.out.println( "|                          |" );
+            System.out.println( "+--------------------------+" );
+
+
+	}
+}
