d639d3fc6b6eb83334456b67f3e760caa5cc43a7
Ryan Grillo <ryangrillo@gmail.com>
2017-03-25 16:48:59 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+package javahard;
+
+public class FirstProg {
+	public static void main(String[] args) {
+		System.out.println( "I Ryan Grillo am determined to learn how to code." );
+		System.out.println( " todays date is 3/22/2017" );
+	}
+}
