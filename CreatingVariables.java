53b9b0a1ee944cddc4c71d3bbeb7c5a9c57f5fa7
Ryan Grillo <ryangrillo@gmail.com>
2017-03-25 16:54:01 -0700

    added exercises

---
+++ CreatingVariables.java
@@ -0,0 +1,35 @@
+package javahard;
+
+public class CreatingVariables {
+     public static void main( String[] args ) {
+         int x, y, z, age;
+         double seconds, e, checking, savings;
+         String firstName, lastName, title, color, month;
+ 
+         x = 10;
+         y = 400;
+         z = 12;
+         age = 39;
+ 
+         seconds = 4.71;
+         e = 2.71828182845904523536;
+         checking = 1.89;
+         savings = 12.2;
+ 
+         firstName = "Graham";
+         lastName = "Mitchell";
+         title = "Mr.";
+         color = "Green";
+         month = "May";
+ 
+         // System.out.println( "The variable x contains " + x );
+//          System.out.println( "The value " + y + " is stored in the variable y." );
+//          System.out.println( "The experiment took " + seconds + " seconds." );
+//          System.out.println( "A favorite irrational # is Euler's number: " + e );
+//          System.out.println( "Hopefully you have more than $" + checking + "!" );
+//          System.out.println( "My name's " + title + " " + firstName + lastName );
+         
+         System.out.println(z + " " + savings + color + month);
+     }
+ }
+
