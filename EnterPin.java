af7f5cdbcd7c0014e1e8c5efe0a0b5441e2b7acb
Ryan Grillo <ryangrillo@gmail.com>
2017-03-26 09:54:29 -0700

    Imporoved exer 25

--- EnterPin.java
+++ EnterPin.java
@@ -7,8 +7,19 @@ public class EnterPin {
 		Scanner keyboard = new Scanner(System.in);
 		int pin, entry;
 		pin = 12345;
+		String password = "java";
 		
 		System.out.println("Welcome to the Bank of Java");
+		System.out.println("Enter a password");
+		password = keyboard.next();
+		
+		while (!password.equals("java")){
+		System.out.println("Incorrect password!!");
+		System.out.println("Try entering your password again");
+		password = keyboard.next();
+		}
+		
+		
 		System.out.println("Enter your pin");
 		entry = keyboard.nextInt();
 		
